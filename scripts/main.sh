05a52cfea4dc329b526593599a5214dc4481368a
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-03-03 16:51:47 +0100

    Load fonts only if they have not yet been loaded

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+if ! [ -d ~/.dotfiles ]; then
+   echo "dotfiles repo were not found under the path ~/.dotfiles"
+   exit 1
+fi
+
 cd ~/.dotfiles
 
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
-
-unzip ~/.dotfiles/.fonts/JetBrainsMono.zip -d ~/.dotfiles/.fonts
-ln -s ~/.dotfiles/.fonts ~/.fonts
-fc-cache -f -v
+if [ -d ~/.dotfiles/.fonts ]; then
+   echo "[Cache] fonts"
+else
+   mkdir ~/.dotfiles/.fonts
+   wget -P ~/.dotfiles/.fonts https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+   unzip ~/.dotfiles/.fonts/JetBrainsMono.zip -d ~/.dotfiles/.fonts
+   ln -s ~/.dotfiles/.fonts ~/.fonts
+   fc-cache -f -v
+fi
 
 echo "Dotfiles installation complete!"

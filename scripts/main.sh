5053db9196403a21c5fc805bd00899976d0746ee
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2024-03-03 16:49:19 +0100

    setup fonts

--- scripts/main.sh
+++ scripts/main.sh
@@ -5,4 +5,9 @@ cd ~/.dotfiles
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
 
+
+unzip ~/.dotfiles/.fonts/JetBrainsMono.zip -d ~/.dotfiles/.fonts
+ln -s ~/.dotfiles/.fonts ~/.fonts
+fc-cache -f -v
+
 echo "Dotfiles installation complete!"

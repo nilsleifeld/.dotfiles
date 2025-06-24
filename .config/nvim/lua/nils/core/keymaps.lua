45130215b0cf7d50d4a0cedd217cec2d2fba17a1
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2025-06-24 20:55:13 +0200

    fix: jump to diagnostics

--- .config/nvim/lua/nils/core/keymaps.lua
+++ .config/nvim/lua/nils/core/keymaps.lua
@@ -16,8 +16,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
+vim.keymap.set("n", "üd", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+vim.keymap.set("n", "+d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
@@ -43,9 +43,3 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- map german keyboard layout to english layout
-vim.keymap.set("n", "ü", "[", { noremap = true, silent = true })
-vim.keymap.set("n", "Ü", "{", { noremap = true, silent = true })
-vim.keymap.set("n", "+", "]", { noremap = true, silent = true })
-vim.keymap.set("n", "*", "}", { noremap = true, silent = true })

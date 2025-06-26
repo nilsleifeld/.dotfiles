75eeecc4677d3f459728e0ce2eaea6a2389aa224
nilsleifeld <65818304+nilsleifeld@users.noreply.github.com>
2025-06-26 10:02:56 +0200

    feat: set icons for diagnostics

--- .config/nvim/lua/nils/plugins/lsp.lua
+++ .config/nvim/lua/nils/plugins/lsp.lua
@@ -2,6 +2,12 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function()
+		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 			callback = function(event)
